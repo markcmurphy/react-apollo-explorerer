5c56a4ef22848329773cf847b47f93317d20e27f
markcmurphy <mark@murphymark.me>
2022-04-05 14:13:59 -0500

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 import { ApolloExplorerReact } from '@apollo/explorer';
-  
+
 export default function App() {
   return (
-    <ApolloExplorerReact 
-      graphRef='My-Graph-aj8l5j@current'
-      endpointUrl='https://bc.murphymark.me/graphql'
+    <ApolloExplorerReact
+      graphRef="My-Graph-aj8l5j@current"
+      endpointUrl="https://bc.murphymark.me/graphql"
       persistExplorerState={true}
-      style={{"height":'100vh'}}
+      style={{ height: '100vh' }}
       initialState={{
-        document: `query ExampleQuery {
-  customer {
-    entityId
-  }
-}
-`,
-        variables: { 'Variable1': 'ExampleValue' },        
-          headers: {
-            authorization: 'Bearer abc123'
-          },
+        document: `query Site {
+          site {
+            products {
+              edges {
+                node {
+                  name
+                }
+              }
+            }
+          }
+        }
+`,        
+        headers: {
+          authorization: 'Bearer abc123',
+        },
         displayOptions: {
-          showHeadersAndEnvVars: true, 
-          docsPanelState: 'open', 
+          showHeadersAndEnvVars: true,
+          docsPanelState: 'open',
           theme: 'dark',
         },
       }}
