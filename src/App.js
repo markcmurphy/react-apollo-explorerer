bd504099a2b4acaf686e4695d88ef79ec8d7103a
Mark Murphy <mark.murphy@bigcommerce.com>
2022-07-08 10:56:43 -0500

    add input field and query param for hostname

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { useEffect, useState }from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
-import { ApolloExplorerReact } from '@apollo/explorer';
+import { ApolloExplorer } from '@apollo/explorer/react';
+import { Formik, Field, Form } from 'formik';
 
 export default function App() {
-  const [token, setToken] = useState('');
+  const [token, setToken] = useState(process.env.REACT_APP_TOKEN);
+
   const [document, setDocument] = useState(`query Site {
     site {
       products {
@@ -16,30 +18,56 @@ export default function App() {
     }
   }
 `);
-//   const queryParams = new URLSearchParams(window.location.search)
-//   const term = queryParams.get("token")
-  
-//   setToken(term);
 
+  const [endpointUrl, setEndpointUrl] = useState('');
 
-  // const location = queryParams.get("location")
+  useEffect(() => {
+    const queryParams = new URLSearchParams(window.location.search);
+    const storefront_hostname = queryParams.get('storefront_hostname');
+    storefront_hostname
+      ? setEndpointUrl(`https://${storefront_hostname}/graphql`)
+      : null;
+  }, []);
 
   return (
-    <ApolloExplorerReact
-      graphRef="My-Graph-aj8l5j@current"
-      endpointUrl="https://bc.murphymark.me/graphql"
-      persistExplorerState={false}
-      initialState={{
-        document: {document},        
-        headers: {
-          authorization: `${token}`,
-        },
-        displayOptions: {
-          showHeadersAndEnvVars: true,
-          docsPanelState: 'open',
-          theme: 'dark',
-        },
-      }}
-    />
+    <>
+      <h1>Storefront GraphQL Playground</h1>
+      <p>Current hostname: {endpointUrl}</p>
+      <Formik
+        initialValues={{
+          storefront_hostname: `${endpointUrl}`,
+        }}
+        onSubmit={async (values) => {
+          setEndpointUrl(`https://${values.storefront_hostname}/graphql`);
+        }}
+      >
+        <Form>
+          <label htmlFor="storefront_hostname">Change hostname:</label>
+          <Field
+            id="storefront_hostname"
+            name="storefront_hostname"
+            placeholder="store-{HASH}.mybigcommerce.com"
+          />
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
+
+      <ApolloExplorer
+        graphRef="My-Graph-aj8l5j@current"
+        endpointUrl={endpointUrl}
+        persistExplorerState={false}
+        initialState={{
+          document: `${document}`,
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+          displayOptions: {
+            showHeadersAndEnvVars: true,
+            docsPanelState: 'open',
+            theme: 'dark',
+          },
+        }}
+      />
+    </>
   );
 }
