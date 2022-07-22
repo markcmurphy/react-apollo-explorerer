db6a422244d8f1d33a629385b0bf777205e3feff
Mark Murphy <mark.murphy@bigcommerce.com>
2022-07-22 11:41:06 -0500

    update style, persist ls, misc

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import { ApolloExplorer } from '@apollo/explorer/react';
 import { Formik, Field, Form } from 'formik';
 
 export default function App() {
-  const [token, setToken] = useState(process.env.REACT_APP_TOKEN);
+  const [token, setToken] = useState('');
+  const [endpointUrl, setEndpointUrl] = useState('');
 
+  // query to use on initial explorer tab load
   const [document, setDocument] = useState(`query Site {
     site {
       products {
@@ -19,43 +21,64 @@ export default function App() {
   }
 `);
 
-  const [endpointUrl, setEndpointUrl] = useState('');
+  useEffect(() => {
+    // if endpointUrl is set in ls, use it
+    const getHostname = window.localStorage.getItem('endpointUrl');
+    getHostname ? setEndpointUrl(getHostname) : null;
+  }, []);
 
   useEffect(() => {
+    // if hostname and token query params are present, use them
     const queryParams = new URLSearchParams(window.location.search);
     const storefront_hostname = queryParams.get('storefront_hostname');
+    const storefront_token = queryParams.get('storefront_token');
     storefront_hostname
       ? setEndpointUrl(`https://${storefront_hostname}/graphql`)
       : null;
+    storefront_token ? setToken(storefront_token) : null;
   }, []);
 
   return (
     <>
-      <h1>Storefront GraphQL Playground</h1>
-      <p>Current hostname: {endpointUrl}</p>
-      <Formik
-        initialValues={{
-          storefront_hostname: `${endpointUrl}`,
-        }}
-        onSubmit={async (values) => {
-          setEndpointUrl(`https://${values.storefront_hostname}/graphql`);
-        }}
-      >
-        <Form>
-          <label htmlFor="storefront_hostname">Change hostname:</label>
-          <Field
-            id="storefront_hostname"
-            name="storefront_hostname"
-            placeholder="store-{HASH}.mybigcommerce.com"
-          />
-          <button type="submit">Submit</button>
-        </Form>
-      </Formik>
+      <div className="top-form">
+        <span className="h1-style">Storefront GraphQL Playground</span>{' '}
+        <span style={{ marginLeft: '2%' }}>
+          Your hostname: {endpointUrl.split('?')[0]}
+        </span>
+        <Formik
+          initialValues={{
+            storefront_hostname: `${endpointUrl.split('?')[0]}`,
+          }}
+          onSubmit={async (values) => {
+            setEndpointUrl(`https://${values.storefront_hostname}/graphql`);
+            // update endpointUrl in ls on submit
+            window.localStorage.setItem(
+              'endpointUrl',
+              `https://${values.storefront_hostname}/graphql`
+            );
+          }}
+        >
+          <Form>
+            <label htmlFor="storefront_hostname">Change hostname:</label>
+            {/* TODO: form validation and sanitizing */}
+            <Field
+              id="storefront_hostname"
+              name="storefront_hostname"
+              placeholder=" store-{HASH}.mybigcommerce.com"
+            />
+            <button type="submit">Submit</button>
+          </Form>
+        </Formik>
+      </div>
 
       <ApolloExplorer
+        // unique if for this schema
         graphRef="My-Graph-aj8l5j@current"
         endpointUrl={endpointUrl}
-        persistExplorerState={false}
+        className="embedded-explorer"
+        // persistExplorerState allows headers, tabs, and variables to persist
+        persistExplorerState={true}
+        includeCookies={true}
         initialState={{
           document: `${document}`,
           headers: {
